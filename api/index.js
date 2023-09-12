0ae9022994d59eaca13ad9c6edefc57b85946822
Marcos-Contreras <marcosalonso2230@gmail.com>
2023-09-12 10:10:09 -0600

    create api directoryand create vercel.json file for vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const routerApi = require('./routes/index');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
